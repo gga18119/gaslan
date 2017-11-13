2755069a1b38cd988d46702d199c9fc9e95234e5
gga18119 <al221310106@gmail.com>
2017-11-13 13:30:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,34 @@
                 <h1> Grupo Aslan</h1>
                 <h2> Tus aliados en la producción de ese evento</h2>
                 <h3> Conoce nuestros servicios </h3>
+                <p>Servicio profesional en la producción de algodones de azúcar, <br>
+                        contamos con puntos de venta al menudeo además atendemos pedidos especiales desde 30 piezas, <br>
+                        y tenemos capacidad para atender eventos hasta 500 pz por hora.<br>
+                        Empresa formal - Facturamos tu evento <br>
+                        En pedidos para eventos y tiendas tenemos diferentes presentaciones, los precios son los siguientes:<br>
+                        -Algodón mini         $6.50 <br>
+                        -Algodón mediano $9 <br>
+                        -Algodón Grande   $15 <br>
+                        -Mega Algodon      $90 <br>
+                        Algodones de sabores en "Vaso frape" ¡Nuevo! <br>
+                        Sabores a escoger mínimo 10 piezas por sabor <br>
+
+                        Algodón bote mini         $6.50 <br>
+                        Algodón bote mediano $9 <br>
+                        Algodón frasco grande $16 <br>
+                        Rojo- Sandia/fresa <br>
+                        Verde-Lima/limón   <br>
+                        Naranja- Naranja       <br>                   
+                        Cafe- Chocolate  <br>
+                        Azul-  Chicle     <br>
+                        Verde agua- Menta <br>
+                        Amarillo- Piña.       <br>
+                        Morado-   Uva.    <br>
+                        Rosa-Cereza/Chicle <br>
+                        Blanco- Coco     <br>
+
+                        <center>Etiquetas impresas personalizadas + $1 por pieza </center>
+                </p>
                 <h3> Cotizador: Algodon de azucar </h3>
 
         </body>
