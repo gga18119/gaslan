b212d12f14b010aada229ec9f8cdcdc3e545d52e
esteban <ekiradg@gmail.com>
2017-11-13 10:11:38 -0600

    Test page

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Grupo Aslan</title>
+	</head>
+	<body>
+		<h1> Grupo Aslan</h1>
+		<h2> Tus aliados en la producción de ese evento</h2>
+		<h3> Cotizador: Algodon de azucar </h3>
+
+	</body>
+</html>
