65c3f663443dec9a4271528fbb74bb53d4471947
gga18119 <al221310106@gmail.com>
2017-11-16 13:13:33 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,57 @@
-<html>
-        <head>
-                <title>Grupo Aslan</title>
-        </head>
-        <body>
-                <h1> Grupo Aslan</h1>
-                <h2> Tus aliados en la producción de ese evento</h2>
-                <h3> Conoce nuestros servicios </h3>
-                <p>Servicio profesional en la producción de algodones de azúcar, <br>
-                        contamos con puntos de venta al menudeo además atendemos pedidos especiales desde 30 piezas, <br>
-                        y tenemos capacidad para atender eventos hasta 500 pz por hora.<br>
-                        Empresa formal - Facturamos tu evento <br>
-                        En pedidos para eventos y tiendas tenemos diferentes presentaciones, los precios son los siguientes:<br>
-                        Algodones clásicos en bolsa (Sabor Tradicional) <br>
-                        Colores a escoger mínimo 10 piezas por color  <br>
-        
-                        -Algodón mini   $6.50 <br>
-                        -Algodón mediano $9 <br>
-                        -Algodón Grande   $15 <br>
-                        -Mega Algodon      $90 <br>
-
-                        Algodones de sabores en "Vaso frape" ¡Nuevo!    Sabores a escoger mínimo 10 piezas por sabor <br>
-
-                        Algodón bote mini  $6.50 <br>
-                        Algodón bote mediano $9 <br>
-                        Algodón frasco grande $16  <br>
-                        
-                        Rojo- Sandia/fresa <br>
-                        Verde-Lima/limón   <br>
-                        Naranja- Naranja       <br>                   
-                        Cafe- Chocolate  <br>
-                        Azul-  Chicle     <br>
-                        Verde agua- Menta <br>
-                        Amarillo- Piña.       <br>
-                        Morado-   Uva.    <br>
-                        Rosa-Cereza/Chicle <br>
-                        Blanco- Coco     <br>
-
-                        <center>Etiquetas impresas personalizadas + $1 por pieza </center>
-                </p>
-                <h3> Cotizador: Algodon de azucar </h3>
-
-        </body>
+<?xml version="1.0" encoding="UTF-8"?><html xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.w3.org/1999/xhtml">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <title>Page Overview - My first Project - Pidoco° Prototype</title>
+      <link rel="SHORTCUT ICON" href="resources/images/favicon.ico" />
+      <link href="resources/css/central.css" rel="stylesheet" type="text/css" />
+      				
+      					<!--[if IE 5]>
+      					<link href="resources/css/patches/patch_central.css" rel="stylesheet" type="text/css" />
+      					<![endif]-->
+      					<!--[if IE 6]>
+      					<link href="resources/css/patches/patch_central.css" rel="stylesheet" type="text/css" />
+      					<![endif]-->
+      					<!--[if IE 7]>
+      					<link href="resources/css/patches/patch_central_ie7.css" rel="stylesheet" type="text/css" />
+      					<![endif]-->
+      				
+      			<script type="text/javascript">
+				function showTable() {
+					document.getElementById("noscript").style.display = "none";
+				}
+			</script></head>
+   <body onload="showTable();">
+      <div id="page">
+         <div id="header">
+            <div id="headline">
+               <h1>My first Project</h1><span xml:space="preserve">Pidoco° Prototype<br />Exported from Pidoco° Project Creator on Nov 16, 2017 7:33:47 PM.</span></div>
+            <div id="logo"></div>
+         </div>
+         <div id="main">
+            <div id="non-ie">
+               <table class="pages-listing full">
+                  <tr class="odd">
+                     <td>Grupo Aslan</td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page694000370-sketchedArial.xhtml">View sketched (Arial)</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page694000370-sketched.xhtml">View sketched</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page694000370-plain.xhtml">View plain</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>cotizador-algodones</td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page571651947-sketchedArial.xhtml">View sketched (Arial)</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page571651947-sketched.xhtml">View sketched</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page571651947-plain.xhtml">View plain</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>login</td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page806976109-sketchedArial.xhtml">View sketched (Arial)</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page806976109-sketched.xhtml">View sketched</a></td>
+                     <td style="white-space: nowrap;"><a class="btn" href="pages/page806976109-plain.xhtml">View plain</a></td>
+                  </tr>
+               </table>
+            </div>
+            <div id="noscript">You have Javascript disabled. Please enable Javascript to see the content of this page. Thanks</div>
+         </div>
+      </div>
+   </body>
 </html>
